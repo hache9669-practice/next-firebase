1fcada6dddf12382035458d8569ad8528c955d82
hache9669 <hm.ck.rc@gmail.com>
2020-03-21 05:19:34 +0900

    コード整理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Head from "next/head";
-import { NextPage, NextPageContext } from "next";
+import { NextPage } from "next";
 import { CountButton } from "../components/CountButton";
 
-type Props = {
-  title: string;
-};
-
-const page: NextPage<Props> = props => {
+const page: NextPage = () => {
   return (
     <>
       <Head>
