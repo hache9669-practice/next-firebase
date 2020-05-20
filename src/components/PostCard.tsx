aa4ec66c5b7b9b01d6b6546f3838de6a28c19625
hache9669 <hm.ck.rc@gmail.com>
2020-05-20 23:34:28 +0900

    削除機能を実装

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { Post } from "../interface/models";
+import { db } from "../lib/db";
 
 type Props = {
   post: Post;
+};
+
+const handleClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+  const id = event.currentTarget.id;
+  db.collection("posts")
+    .doc(id)
+    .delete()
+    .then(() => {
+      console.log("delete complete: " + id);
+    })
+    .catch(err => {
+      console.log(JSON.stringify(err));
+    });
 };
 
 const component: React.FC<Props> = props => {
@@ -10,6 +24,9 @@ const component: React.FC<Props> = props => {
     <div id={props.post.id}>
       <h3>{props.post.title}</h3>
       <p>{props.post.body}</p>
+      <button id={props.post.id} onClick={handleClick}>
+        delete
+      </button>
     </div>
   );
 };
